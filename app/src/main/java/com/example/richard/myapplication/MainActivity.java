931fdb69a725a4c598dc1edabda2e8ca344b5edf
Richard Sun <richardsundivine@gmail.com>
2018-08-26 20:15:03 +1000

    mesibo skeleton

---
+++ app/src/main/java/com/example/richard/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.richard.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import com.mesibo.api.Mesibo;
+
+
+public class MainActivity extends AppCompatActivity implements Mesibo.MessageListener {
+    public static final String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Mesibo api = Mesibo.getInstance();
+        api.init(this);
+
+        api.addListener(this);
+
+        // set user authentication token obtained by creating user
+        api.setAccessToken("3c1c4711f7617c56b151553b7e63bde5da7bbe119e");
+        api.start();
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean Mesibo_onMessage(Mesibo.MessageParams messageParams, byte[] bytes) {
+        return false;
+    }
+
+    @Override
+    public void Mesibo_onMessageStatus(Mesibo.MessageParams messageParams) {
+
+    }
+
+    @Override
+    public void Mesibo_onActivity(Mesibo.MessageParams messageParams, int i) {
+
+    }
+
+    @Override
+    public void Mesibo_onLocation(Mesibo.MessageParams messageParams, Mesibo.Location location) {
+
+    }
+
+    @Override
+    public void Mesibo_onFile(Mesibo.MessageParams messageParams, Mesibo.FileInfo fileInfo) {
+
+    }
+}
